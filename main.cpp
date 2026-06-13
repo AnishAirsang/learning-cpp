3c1fd638f8787b4393f2b6bdbbaa08991d3813fd
AnishAirsang <airsanganish@gmail.com>
2026-06-13 06:54:22 +0000

    If, Else, Else if, did 3 questions

--- main.cpp
+++ main.cpp
@@ -137,4 +137,59 @@ int main(){
     int cookies_left = cookies % friends;
 
     cout << "Total cookies left over: " << cookies_left << endl;
+
+    //if
+
+    //general format:
+   // if (condition) {
+        //code to run if the condition is true
+    //}
+
+    int score= 85;
+    if (score >= 50){
+        cout << "you passed \n";
+
+    }
+    // Tiny Tiny Project: Driving Age Checker
+    int driver_age;
+    cout << "Enter your age: \n";
+    cin >> driver_age;
+
+    if (driver_age >= 18){
+    cout << "You are eligible to drive";
+    } else {
+        cout << "You are not eligible to frive";
+    }
+
+    // Tiny Tiny Project: Check if a student passed or failed a test (passing is 50+)
+    int marks;
+    cout << "Enter your marks: \n";
+    cin >> marks;
+
+    if (marks >= 50){
+        cout << "You passed";
+    } else {
+        cout << "you failed";
+    }
+
+    //else if type questions
+
+    int temp;
+    cout << "Enter temperature: \n";
+    cin >> temp;
+
+    if (temp > 90){
+        cout << "very hot \n";
+    } else if (temp > 60){
+        cout << "normal temp \n";
+    } else {
+        cout << "very cold \n";
+    }
+    
+
+
+
+
+
+
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int driver_age;
+    cout << "Enter your age: \n";
+    cin >> driver_age
+
+    if (driver_age >= 18);
+    cout << "You are eligible to drive"
